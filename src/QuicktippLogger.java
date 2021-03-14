2ac5a4e3683d095f030d558fac3c9ef2e8fcfa71
Dennis Ziebart <ziebartdennis@gmail.com>
2021-03-14 18:21:23 +0100

    Formatter added to QuicktippLogger

--- src/QuicktippLogger.java
+++ src/QuicktippLogger.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class QuicktippLogger {
 		
@@ -15,6 +16,8 @@ public class QuicktippLogger {
 			fh = new FileHandler("QuicktippLogger.log", false);
 			fh.setLevel(Level.FINE);
 			logger.addHandler(fh);
+			SimpleFormatter formatter = new SimpleFormatter();  
+	        fh.setFormatter(formatter);  
 		} catch (IOException e) {
 			logger.log(Level.SEVERE, "File Logger not working.", e);
 		}
