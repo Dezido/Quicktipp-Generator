b2d2982ebac26303b06075c445725dac2d886929
Dennis Ziebart <ziebartdennis@gmail.com>
2021-03-15 21:56:47 +0100

    added backup and recovery of Ünglückszahlen

--- src/Quicktipp.java
+++ src/Quicktipp.java
@@ -1,3 +1,8 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.util.Scanner;
 import java.util.logging.Logger;
 
@@ -7,20 +12,24 @@ public class Quicktipp {
 
 	public static void main(String[] args) {
 		menu(parseArgs(args));
-
 	}
 
 	public static int[] parseArgs (String [] args) {
-
 		int [] unglueckszahlen = new int [args.length];
-
 		for(int i=0;i<args.length;i++) {
 			unglueckszahlen[i]=Integer.parseInt(args[i]);
 		}
 		return unglueckszahlen;		
 	}
 
-
+	public static boolean checkParsedArgs (int [] unglueckszahlen) {		
+		for(int i=0; i<unglueckszahlen.length;i++) {
+			if(unglueckszahlen[i]<0||unglueckszahlen[i]>50) {
+				return false;
+			}
+		}
+		return true;
+	}
 
 	public static void lottoQuicktipp(int [] unglueckszahlen) {		
 		Lotto lotto = new Lotto(6,49);
@@ -46,72 +55,145 @@ public class Quicktipp {
 		System.out.println("Danke für die Nutzung des Quicktipp-Generators");
 	}
 
-	public static void changeUnglueckszahlen() {
-		//TODO
-	}
-	
-	public static void deleteUnglueckszahlen(){
-		//TODO
+	public static boolean filePresent() {
+		File f = new File("Unglueckszahlen.txt");
+		if(f.exists() && !f.isDirectory()) { 
+		    return true;
+		}
+		try {
+			f.createNewFile();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return false;
 	}
 
-	//Hauptmenü
+	public static void deleteUnglueckszahlen(){
+		try {
+			PrintWriter pw = new PrintWriter("Unglueckszahlen.txt");
+			pw.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public static void saveUnglueckszahlen(int [] unglueckszahlen) {
+		try {
+			PrintStream printOut = new PrintStream("Unglueckszahlen.txt"); //Unglückszahlen als Textdatei speichern
+			for(int i=0;i<unglueckszahlen.length;i++) {  
+				printOut.println(unglueckszahlen[i]);
+			}
+			printOut.close();
+		} catch (FileNotFoundException e) {
+			System.out.println("Die Datei zum speichern der Unglückszahlen ist nicht vorhanden");
+			e.printStackTrace();
+		}				
+	}
+
+	public static int [] loadUnglueckszahlen () {
+		int [] unglueckszahlen= new int [howManySaved()];
+		try {
+			Scanner fileIn = new Scanner (new File("Unglueckszahlen.txt"));
+			for(int i=0;i<howManySaved();i++) {
+					unglueckszahlen[i]=fileIn.nextInt();
+					}
+			} catch (FileNotFoundException e) {
+				System.out.println("Die Datei zum laden der Unglückszahlen ist nicht vorhanden");
+				e.printStackTrace();
+			}
+		return unglueckszahlen;
+	}
+	
+	public static int howManySaved () {
+		int count=0;
+		try {
+			Scanner fileIn = new Scanner (new File("Unglueckszahlen.txt"));
+				while(fileIn.hasNext()) {
+					count++;
+					fileIn.next();
+				}				
+					} catch (FileNotFoundException e) {
+				System.out.println("Die Datei zum laden der Unglückszahlen ist nicht vorhanden");
+				e.printStackTrace();
+			}
+		return count;
+	}
+
+
 	private static void menu (int [] unglueckszahlen) {
-		System.out.println("Guten Tag, Sie haben folgende Optionen:");
-		System.out.println("1: Lotto (6aus49) Quicktipp erstellen");
+		
+		filePresent();
+		
+		if(!checkParsedArgs(unglueckszahlen)) {
+			System.out.println("Die übergebenen Unglückszahlen sind ungültig. Es können maximal 6 Zahlen zwischen 0 und 50 verwendet werden");
+			System.out.println("Rufen Sie bitte die Anwendung erneut mit gültigen Ünglückszahlen auf");
+			System.exit(0);
+		}
+		if(unglueckszahlen.length==0) {
+			if(unglueckszahlenSaved()) {					
+				System.out.println("Sie haben keine neuen Ünglückszahlen übergeben");
+				System.out.println("Es werden die gespeicherten Unglückszahlen verwendet");
+				unglueckszahlen=loadUnglueckszahlen();
+			}else {
+				System.err.println("Es sind keine gespeicherten Unglückszahlen vorhanden");
+				System.out.println("Rufen Sie bitte die Anwendung erneut mit gültigen Ünglückszahlen auf");
+				System.exit(0);
+			}
+		}		
+		System.out.println("Folgende Unglückszahlen werden verwendet: ");
+		for(int i=0; i<unglueckszahlen.length;i++) {
+			if(unglueckszahlen[i]!=-1) {
+				System.out.print(unglueckszahlen[i]+ " ");
+			}
+		}		
+		System.out.println();
+		System.out.println("Sie haben folgende Optionen:");
+		System.out.println();
+		System.out.println("1: Lotto 6aus49 Quicktipp erstellen");
 		System.out.println("2: Eurojackpot Quicktipp erstellen");
-		System.out.println("3: Unglückszahlen bearbeiten");
-		System.out.println("4: Programm beenden");
+		System.out.println("3: Gespeicherte Unglückszahlen löschen");
+		System.out.println("4: Programm beenden");						
 		Scanner keyboard = new Scanner(System.in);
 		int option = keyboard.nextInt();			
 		while (option <0||option >4) {
-			System.out.println("Falsche Eingabe \nDie Optionen lauten 1,2,3 oder 4.");
+			System.out.println("Falsche Eingabe \nDie Optionen lauten 1,2,3 oder 4");
 			option = keyboard.nextInt();			
 		}
 		switch (option) {
 		case 1:
 			lottoQuicktipp(unglueckszahlen);
+			saveUnglueckszahlen(unglueckszahlen);
 			System.exit(0);
 		case 2:
 			eurojackpotQuicktipp(unglueckszahlen);
+			saveUnglueckszahlen(unglueckszahlen);
 			System.exit(0);
 		case 3:
-			submenu(unglueckszahlen);
+			if(!unglueckszahlenSaved()) {
+				System.out.println("Es sind keine gespeicherten Unglückszahlen vorhanden");
+				System.out.println("Bitte wählen Sie eine andere Option");
+			}else {
+			deleteUnglueckszahlen();
+			System.out.println("Die gespeicherten Unglückszahlen wurden gelöscht");
+			System.out.println("Programm wird beendet");
+			System.out.println("Danke für die Nutzung des Quicktipp-Generators");
+			keyboard.close();
+			System.exit(0);
+			}			
 		case 4:
 			System.out.println("Programm wird beendet");
 			System.out.println("Danke für die Nutzung des Quicktipp-Generators");
 			keyboard.close();
 			System.exit(0);
 		}
-
-
 	}
 
-	//Untermenü zur Änderung der Unglückszahlen
-	private static void submenu (int [] unglueckszahlen) {
-		System.out.println("Welche Änderungen möchten Sie durchführen?");
-		System.out.println("1: Gespeicherte Unglückszahlen bearbeiten");
-		System.out.println("2: Gespeicherte Unglückszahlen löschen");
-		System.out.println("3: Zurück zum Hauptmenü");
-		Scanner keyboard = new Scanner(System.in);
-		int option = keyboard.nextInt();	
-		while (option <0||option >3) {
-			System.out.println("Falsche Eingabe \nDie Optionen lauten 1,2 oder 3.");
-			option = keyboard.nextInt();			
-		}
-		switch (option) {
-		case 1:
-			System.out.println("Folgende Zahlen wurden gespeichert: ");
-			changeUnglueckszahlen();
-			System.exit(0);
-		case 2:
-			System.out.println("Die gespeicherten Zahlen wurden gelöscht");
-			deleteUnglueckszahlen();
-			System.exit(0);
-		case 3:
-			menu(unglueckszahlen);		
-		}
-		keyboard.close();
-		System.exit(0);
+	private static boolean unglueckszahlenSaved() {
+		int [] unglueckszahlen = loadUnglueckszahlen();
+		if(unglueckszahlen.length==0) {
+			return false;
+		}	
+		return true;
 	}
-
 }
